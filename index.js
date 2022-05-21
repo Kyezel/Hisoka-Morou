d3ae9cd0b5568eb5156c7babf110912065fe6c14
Kyezel <hazeluki67@gmail.com>
2022-05-21 09:33:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
-   * Create By Dika Ardnt.
-   * Contact Me on wa.me/6288292024190
-   * Follow https://github.com/DikaArdnt
+   * Create By Kyexi.
+   * Contact Me on wa.me/6283862627180
+   * Follow https://github.com/Kyezel
 */
 
 require('./config')
